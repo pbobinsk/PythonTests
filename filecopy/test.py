d123f9521002878f69b7f713fbb141b15a442d81
pbobinsk <pbobinsk@gmail.com>
2024-11-07 16:10:01 +0000

    działa z docelowymi metodami

--- filecopy/test.py
+++ filecopy/test.py
@@ -1,5 +1,9 @@
-import shutil
+import shutil, logging
 
 #shutil.copyfile('../source/WR_S0001_Z05BO.wav', '../dest/WR_S0001_Z05BO.wav')
 
-shutil.copy('../source/WR_S0001_Z05BO.wav', '../dest/')
+try:
+    shutil.copy('../source/WR_S0001_Z05BO.wav', '../dest/')
+except Exception as e:
+    logging.error('Error',e)
+
--- simpleJSONrpc/clientASR.py
+++ simpleJSONrpc/clientASR.py
@@ -2,7 +2,7 @@ from jsonrpcclient import request, parse, Ok, Error
 import logging
 import requests
 
-response = requests.post("http://localhost:5666/", json=request("ping"))
+""" response = requests.post("http://localhost:5666/", json=request("ping"))
 match parse(response.json()):
     case Ok(result, id):
         print(result)
@@ -16,8 +16,24 @@ match parse(response.json()):
         print(result)
     case Error(code, message, data, id):
         logging.error(message)
+ """
+response = requests.post("http://localhost:5666/", json=request("uploadFile",params={"path":r"C:\Users\DELL\venvTests\source\test1.wav"}))
+print(response.json())
+match parse(response.json()):
+    case Ok(result, id):
+        print(result)
+    case Error(code, message, data, id):
+        logging.error(message)
 
-response = requests.post("http://localhost:5666/", json=request("hello",params={"name":"dupa"}))
+response = requests.post("http://localhost:5666/", json=request("uploadFile",params={"path":r"C:\Users\DELL\venvTests\source\test1_doctor.wav"}))
+print(response.json())
+match parse(response.json()):
+    case Ok(result, id):
+        print(result)
+    case Error(code, message, data, id):
+        logging.error(message)
+
+response = requests.post("http://localhost:5666/", json=request("doASR",params={"file":"test1.wav","file_doctor":"test1_doctor.wav"}))
 print(response.json())
 match parse(response.json()):
     case Ok(result, id):
