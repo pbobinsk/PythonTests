c5d65a585b46d7bc01588c492c464d04b2fc8482
pbobinsk <pbobinsk@gmail.com>
2024-11-07 16:29:18 +0000

    metoda do zdalnych wywołań

--- simpleJSONrpc/clientASR.py
+++ simpleJSONrpc/clientASR.py
@@ -2,41 +2,17 @@ from jsonrpcclient import request, parse, Ok, Error
 import logging
 import requests
 
-""" response = requests.post("http://localhost:5666/", json=request("ping"))
-match parse(response.json()):
-    case Ok(result, id):
-        print(result)
-    case Error(code, message, data, id):
-        logging.error(message)
+def call(jsonRequest):
+    response = requests.post("http://localhost:5666/", json=jsonRequest)
+    match parse(response.json()):
+        case Ok(result, id):
+            print(result)
+        case Error(code, message, data, id):
+            logging.error(message)
 
-response = requests.post("http://localhost:5666/", json=request("hello",params={"name":"Piotr"}))
-print(response.json())
-match parse(response.json()):
-    case Ok(result, id):
-        print(result)
-    case Error(code, message, data, id):
-        logging.error(message)
- """
-response = requests.post("http://localhost:5666/", json=request("uploadFile",params={"path":r"C:\Users\DELL\venvTests\source\test1.wav"}))
-print(response.json())
-match parse(response.json()):
-    case Ok(result, id):
-        print(result)
-    case Error(code, message, data, id):
-        logging.error(message)
+call(request("ping"))
+call(request("hello",params={"name":"Piotr"}))
+call(request("uploadFile",params={"path":r"C:\Users\DELL\venvTests\source\test1.wav"}))
+call(request("uploadFile",params={"path":r"C:\Users\DELL\venvTests\source\test1_doctor.wav"}))
+call(request("doASR",params={"file":"test1.wav","file_doctor":"test1_doctor.wav"}))
 
-response = requests.post("http://localhost:5666/", json=request("uploadFile",params={"path":r"C:\Users\DELL\venvTests\source\test1_doctor.wav"}))
-print(response.json())
-match parse(response.json()):
-    case Ok(result, id):
-        print(result)
-    case Error(code, message, data, id):
-        logging.error(message)
-
-response = requests.post("http://localhost:5666/", json=request("doASR",params={"file":"test1.wav","file_doctor":"test1_doctor.wav"}))
-print(response.json())
-match parse(response.json()):
-    case Ok(result, id):
-        print(result)
-    case Error(code, message, data, id):
-        logging.error(message)
