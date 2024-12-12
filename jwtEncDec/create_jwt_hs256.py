ad92e9a5a6fe9dff22c0aef05ae6a50e0985e1e3
pbobinsk <pbobinsk@elektron.pw.edu.pl>
2024-12-12 09:10:27 +0100

    exp date

--- jwtEncDec/create_jwt_hs256.py
+++ jwtEncDec/create_jwt_hs256.py
@@ -1,12 +1,19 @@
 import jwt
+from datetime import date, datetime, timedelta
+
+dt=datetime.utcnow()+timedelta(seconds=120)
+
+print(dt)
 
 
 payload_data = {
     'sub': '4242',
     'name': 'Jessica Temporal',
-    'nickname': 'Jess'
+    'nickname': 'Jess',
+    'exp':dt
 }
 
 secret = 'my_super_secret'
 token = jwt.encode(payload=payload_data, key=secret)
 print(token)
+
--- jwtEncDec/verify_jwt_hs256.py
+++ jwtEncDec/verify_jwt_hs256.py
@@ -1,7 +1,8 @@
 import jwt
 
 
-token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI0MjQyIiwibmFtZSI6Ikplc3NpY2EgVGVtcG9yYWwiLCJuaWNrbmFtZSI6Ikplc3MifQ.EDkUUxaM439gWLsQ8a8mJWIvQtgZe0et3O3z4Fd_J8o'
+token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI0MjQyIiwibmFtZSI6Ikplc3NpY2EgVGVtcG9yYWwiLCJuaWNrbmFtZSI6Ikplc3MiLCJleHAiOjE3MzM5OTA5OTB9.4_4ZnhA0qNKx_kCWuMTwfkcfMf2WA2V2mLE2a85xzv0'
+
 secret = 'my_super_secret'
 header_data = jwt.get_unverified_header(token)
 
