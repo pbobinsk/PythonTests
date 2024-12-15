db97a0d374544c824d01d634f7d6b3c759ef8706
pbobinsk <pbobinsk@gmail.com>
2024-12-15 19:27:18 +0100

    klucze generowane w jawie

--- jwtEncDec/create_jwt_rs256_ssh.py
+++ jwtEncDec/create_jwt_rs256_ssh.py
@@ -1,6 +1,7 @@
 import jwt
 
 from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.backends import default_backend
 
 
 payload_data = {
@@ -10,8 +11,10 @@ payload_data = {
 }
 
 # you'll need to create or update the path to correspond to an available key
-private_key = open('../../.ssh/id_rsa', 'r').read()
-key = serialization.load_ssh_private_key(private_key.encode(), password=b'qazxsw')
+#private_key = open('./.keys/id_rsa', 'r').read()
+private_key = open('./.keys/private_key_pkcs8.pem', 'rb').read()
+#key = serialization.load_ssh_private_key(private_key.encode(), password=b'qazxsw')
+key = serialization.load_pem_private_key(private_key,password=None,backend=default_backend())
 
 token = jwt.encode(payload=payload_data, key=key, algorithm='RS256')
 print(token)
